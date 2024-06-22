9e82e3508a76f36dfc3a56c3191e23438d05e186
MUNEEBA MEHMOOD <124117980+engrmuneebamehmood@users.noreply.github.com>
2024-06-22 16:29:59 +0500

    Update settings.py

--- Amna/settings.py
+++ Amna/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n%+i(ir-hl(o(optggdxgc)@^5e=)i0=o87hk)dgl81o!8fh+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,7 +74,12 @@ WSGI_APPLICATION = 'Amna.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # settings.py
 
